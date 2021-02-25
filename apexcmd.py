7c82cd9fcce23b2583dc59836272b48ff1870b4b
Altan Stalker <general@aaarfamily.com>
2021-02-24 19:22:21 -0500

    added support for apexcmd.yaml

--- apexcmd.py
+++ apexcmd.py
@@ -6,6 +6,7 @@ import argparse
 import socket
 import json
 import logging
+import yaml
 
 ##
 ## Code
@@ -20,45 +21,77 @@ def apexCmdMain():
     parser.add_argument("--port", "-p", help="Specify port")
     parser.add_argument("--ip", "-ip", help="Specify IP address")
     parser.add_argument("--secret", "-s", help="Specify secret")
+    parser.add_argument("--configfile", "-cf", help="Specify location of config file")
+    parser.add_argument("--noconfigfile", "-ncf", action='store_true', help="Specify all parameters are on command line")
 
     args = parser.parse_args()
 
-    if not args.ip:
+    cfgName = 'apexcmd.yaml'
+
+    if args.noconfigfile:
+        cfgName = None
+
+    if args.configfile:
+        # user has specified a locaiton for the config file
+        cfgName = args.configfile
+
+    cfg = {}
+    if cfgName:
+        # read config
+        with open(cfgName) as file:
+            cfg = yaml.full_load(file)
+
+    if args.ip:
+        cfg['ip'] = args.ip
+
+    if args.port:
+        cfg['port'] = int(args.port)
+
+    if args.secret:
+        cfg['secret'] = args.secret
+
+    # now check we have acceptable parameters
+    if not cfg.get('ip'):
         print('Must supply an IP address')
         return
 
-    if not args.port:
+    if not cfg.get('port'):
         print('Must supply a Port')
         return
 
-    if not args.secret:
+    if not cfg.get('secret'):
         print('Must specify a secret')
         return
 
-    print('Creating socket...')
+    if (not args.rccode) and (not args.profile):
+        print('Need to specify something to send')
+        return
+
+    #print('Creating socket...')
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
-        print('Calling connect')
-        parms = (args.ip, int(args.port) )
-        print(parms)
+        parms = (cfg['ip'], cfg['port'] )
+        # print(parms)
         sock.connect( parms )
 
         if args.rccode:
             msg = {}
-            msg['secret'] = args.secret
+            msg['secret'] = cfg['secret']
             msg['rccode'] = args.rccode
             js = json.dumps(msg)
-            print(js)
+            #print(js)
             sock.sendall(bytes(js,'utf-8'))
+            print(f"Apex rccode {msg['rccode']} sent")
 
         if args.profile:
             msg = {}
-            msg['secret'] = args.secret
+            msg['secret'] = cfg['secret']
             msg['profile'] = args.profile
             js = json.dumps(msg)
-            print(js)
+            #print(js)
             sock.sendall(bytes(js,'utf-8'))
+            print(f"Apex profile {msg['profile']} sent")
 
 
 if __name__ == "__main__":
