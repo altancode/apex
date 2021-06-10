e930d6cbd137135ce99c6027118b6640165437fe
Altan Stalker <general@aaarfamily.com>
2021-06-10 16:14:07 -0400

    added support for Frame Adapt HDR, HLG and PanaPQ

--- misc.py
+++ misc.py
@@ -7,17 +7,21 @@ pmLookup = {
     b'01': 'Cinema',
     b'02': 'Animation',
     b'03': 'Natural',
+    b'04': 'HDR10'
     b'06': 'THX',
+    b'0B': 'FrameAdaptHDR'
     b'0C': 'User1',
     b'0D': 'User2',
     b'0E': 'User3',
     b'0F': 'User4',
     b'10': 'User5',
-    b'11': 'User6'
+    b'11': 'User6',
+    b'14': 'HLG',
+    b'16': 'PanaPQ'
 }
 
 def getPictureMode(bin):
     if bin in pmLookup:
         return pmLookup[bin]
     else:
-        return 'Unknown ' + str(bin)
+        return 'Unknown' + bin.decode('ascii')
