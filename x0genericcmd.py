86d3a3c5db0f11bae647acb8de98b74dbd6e52ee
Altan Stalker <general@aaarfamily.com>
2021-09-16 10:01:25 -0400

    added close on complete and improved timeout when connect is failing

--- x0genericcmd.py
+++ x0genericcmd.py
@@ -13,7 +13,7 @@ import misc
 class X0GenericCmd(ABC):
     """The main Apex state object that does the magic"""
 
-    def __init__(self, inComm, useLog, timeoutConfig):
+    def __init__(self, inComm, useLog, timeoutConfig, closeOnComplete = True):
 
         self.log = useLog
         self.desired = None
@@ -26,9 +26,10 @@ class X0GenericCmd(ABC):
         self.opcmd = b''
         self.chatty = False
         self.target = None
+        self.closeOnComplete = closeOnComplete
 
-        global log
-        log = self.log
+        self.log.debug(f'refAckOffset {self.refAckOffset}, cmdRetries {self.cmdRetries}')
+
 
     @abstractmethod
     def makeDesired(self, cmd, data):
@@ -92,85 +93,128 @@ class X0GenericCmd(ABC):
         # this is where the work happens
 
         if self.state == '':
-            # no unexpected data coming from source so we start
-            log.debug(f'Inside state "{self.state}"')
+            self.log.debug(f'Inside state "{self.state}" {self.checkwaitacktimeout}')
 
-            # we convert it into byte
-#            self.opcmd = self.desired + b'\x0a'
             self.opcmd = self.makeOpCmd()
-            log.debug(f'Sending command {self.opcmd}')
+
+            self.state = 'waitACK'
+            print(f'Setting timeout')
+            self.timeout = time.time() + self.refAckOffset    
+
+            self.state = 'sendCmd'
+
+        ## this is intentionally an if and not an elif
+        ## we want the code above to potentially trigger this code below
+
+        if self.state == 'sendCmd':
+            self.log.debug(f'Inside state "{self.state}" {self.checkwaitacktimeout}')
+
+            self.log.debug(f'Sending command {self.opcmd}')
 
             # send the command
             ok = self.comm.send(self.opcmd)
+            print(f'OK is {ok}')
             if not ok:
                 # do not move to next state!
                 # basically we'll try again next time
-                log.debug(f'Could not send command')
+                self.log.debug(f'Could not send command')
+                if time.time() > self.timeout:
+                    # oops.  Start over
+                    self.log.debug(f'Trying to send Timeout in {self.state}.  {self.checkwaitacktimeout}.  Starting over')
+                    self.state = ''
+
+                    self.checkwaitacktimeout += 1
+                    if self.checkwaitacktimeout > self.cmdRetries:
+                        # we just give up
+                        self.log.warning(f'Giving up...')
+                        self.desired = None
+                        self.checkwaitacktimeout = 0
+
             else:
                 # it was sent
                 self.state = 'waitACK'
-                self.timeout = time.time() + self.refAckOffset
+#                self.timeout = time.time() + self.refAckOffset
         
         elif self.state == 'waitACK':
-            log.debug(f'Inside state "{self.state}"')
+            self.log.debug(f'Inside state "{self.state}" {self.checkwaitacktimeout}')
 
             # quick test of whether socket is connected
             ok = self.comm.send(b'')
             if not ok:
                 # no socket even after send tried to create one
-                log.debug(f'connection lost and not ready yet.  Restarting...')
-                self.state = ''
+                self.log.debug(f'connection lost and not ready yet.  Restarting...')
 
+                # got nothing.  Have we timed out?
+                if time.time() > self.timeout:
+                    # oops.  Start over
+                    self.log.debug(f'Connection Loss and Timeout in {self.state}.  {self.checkwaitacktimeout}.  Starting over')
+                    self.state = ''
+
+                    self.checkwaitacktimeout += 1
+                    if self.checkwaitacktimeout > self.cmdRetries:
+                        # we just give up
+                        self.log.warning(f'Giving up...')
+                        self.desired = None
+                        self.checkwaitacktimeout = 0
+                else:
+                    self.state = 'sendCmd'
+
+
+            # sent ok
             else:
                 # see if there's a response
                 rxData = self.comm.read()
 
                 if rxData == b'':
-                    log.debug(f'Got no data')
+                    self.log.debug(f'Got no data')
                     # got nothing.  Have we timed out?
                     if time.time() > self.timeout:
                         # oops.  Start over
-                        log.debug(f'Timeout in {self.state}.  {self.checkwaitacktimeout}.  Starting over')
+                        self.log.debug(f'Timeout in {self.state}.  {self.checkwaitacktimeout}.  Starting over')
                         self.state = ''
 
                         self.checkwaitacktimeout += 1
                         if self.checkwaitacktimeout > self.cmdRetries:
                             # we just give up
-                            log.warning(f'Giving up...')
+                            self.log.warning(f'Giving up...')
                             self.desired = None
                             self.checkwaitacktimeout = 0
 
                 else:
-                    log.debug(f'Got data {rxData}')
+                    self.log.debug(f'Got data {rxData}')
                     # we got something
                     self.checkwaitacktimeout = 0
 
                     if self.isMatchingAck(rxData,self.opcmd):
-#                    if self.makeMatchingAck(rxData) == self.target:
-#                    if rxData.split(b' ')[0] == self.target:
                         # got the ack
-                        log.debug(f'Got the ACK {rxData}')
+                        self.log.debug(f'Got the ACK {rxData}')
 
                         self.state = ''
                         self.desired = None
 
-                        ### THIS IS WRONG
-                        ### IT IS RETURNING A LIST
                         result = self.makeCmdResult(rxData)
-#                        result = b''.join(rxData.split(b' ')[1:])
-#                        result = result.replace(b'\r\n',b'')
+
+                        if self.closeOnComplete:
+                            self.log.debug(f'closing connection because closeOnComplete is {self.closeOnComplete}')
+                            self.comm.close()
+
                         return (True, result)
 
                     else:
                         # what happened?
-                        log.warning(f'Did not receive expected results with {rxData}.  Ignoring...')
+                        self.log.warning(f'Did not receive expected results with {rxData}.  Ignoring...')
 
         else:
-            log.error('**** YIKES {self.state}')
+            self.log.error('**** YIKES {self.state}')
             
 
         if not self.desired:
             # we are done
+            if self.closeOnComplete:
+                self.log.debug(f'closing connection because closeOnComplete is {self.closeOnComplete}')
+                self.comm.close()
+
+            # return that we are done with no result
             return (True,None)
         else:
             # more to do
@@ -187,10 +231,10 @@ class X0GenericCmd(ABC):
 #        print(f'combined {combined}')
         if self.desired:
             # same mode
-            log.warning(f'!!!! Already sending command {combined}')
+            self.log.warning(f'!!!! Already sending command {combined}')
         else:
             self.desired = combined
-            log.debug(f'Asked to send command {combined}')
+            self.log.debug(f'Asked to send command {combined}')
 
             # definitely need to restart the state machine
             self.state = ''
